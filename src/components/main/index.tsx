e7f0749d7d39478a00e4278890e0ad2cc7a906c5
Norbert Stange <norberto.stange@est.fi.uncoma.edu.ar>
2021-05-21 12:39:05 -0300

    Update de index

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-
+import './main.scss';
 const Main = () => {
     return (
-        <div className="row mt-5">
+        <div className="row mt-4">
             <div className="col"></div>
-            <div className="col-md-6 d-flex flex-column justify-content-center buttons-container">
-                <Link to="/table" className="btn btn-outline-primary btn-block mb-3 font-weight-bold busqueda-button">Búsqueda por DNI/CUIT</Link>
+            <div className="col-md-6 p-3 background-main-div">
+                <h2 className="text-primary mb-3 text-left">LOS TRIBUTOS RELACIONADOS A SU DNI / CUIT SON</h2>
 
-                <Link to="/apps/totems/tributo" type="button" className="btn btn-outline-primary btn-block font-weight-bold busqueda-button">Búsqueda por TRIBUTO</Link>
+                <div className="col-md-6 d-flex flex-column justify-content-center pull-left">
+                    <a href="rodados.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Rodados
+                    </a>
+                    <a href="comercio.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Comercio
+                    </a>
+                    <a href="planes-pago.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Planes de pago
+                    </a>
+                    <a href="venta-tierras-fiscales.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Ventas de Tierras Fiscales
+                    </a>
+                </div>
+
+                <div className="col-md-6 d-flex flex-column justify-content-center pull-left">
+                    <a href="inmuebles.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Inmuebles
+                    </a>
+                    <a href="cementerio.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Cementerio
+                    </a>
+                    <a href="recupero-plan-federal.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Recupero Plan Federal
+                    </a>
+                    <a href="residuos-patogenos.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Residuos Patógenos
+                    </a>
+                    <a href="natatorios.html" type="button" className="btn btn-primary btn-labeled text-left mb-1">
+                        <span className="btn-label"><i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span>Natatorios
+                    </a>
+
+                </div>
+                <a href="busqueda-dni.html" type="button" className="btn btn-primary active mb-1">
+                    <i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i> Volver
+                </a>    
             </div>
             <div className="col"></div>
         </div>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 
-import Main from '../components/Main/index';
+import Main from '../components/main';
 import Tributo from '../components/busqueda-tributo';
 
 const App = () => {
