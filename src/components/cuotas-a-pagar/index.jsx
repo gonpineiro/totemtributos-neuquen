6b6937de650f7dc104656db5f037dd3e803cf12d
Norbert Stange <n0rb3r7@outlook.com>
2021-05-24 14:00:00 -0300

    Visualización error cuando el imponible no existe.
    - Arreglo del header.

--- src/components/cuotas-a-pagar/index.jsx
+++ src/components/cuotas-a-pagar/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import { Link } from 'react-router-dom';
 import { imponible } from './ctaCtoAxios';
 
 import './cuotas-a-pagar.scss';
@@ -23,7 +23,21 @@ export const App = ({
     console.log(datos);
 
     if (datos == null) return 'Cargando';
-    if (datos.error) return 'Error';
+    if (datos.error) return (
+        <div className="row mt-4">
+            <div className="col-md-6 offset-md-3 p-3 text-center background-main-div">
+                <h2 className="text-primary mb-3 text-center">Error</h2>
+                <p className="titulo text-center">{datos.error}</p>
+            </div>
+            <div className='col-md-6 offset-md-3 pt-3 text-center'>
+                <Link to="/apps/totems" className="btn btn-primary active mb-1 pull-left">
+                    <i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i>
+                    Volver
+                </Link>
+            </div>
+        </div>
+                
+    );
 
     const Row = (id, estado_d) => (
         <tr>
