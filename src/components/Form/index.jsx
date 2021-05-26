ee70feb8b663cb4b384d51a1b825a3ac75419cd9
Norbert Stange <n0rb3r7@outlook.com>
2021-05-26 13:34:52 -0300

    Update form
    - Se usa una card como en el home (todos los impuestos)

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,53 +1,108 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-import './styles.scss';
+import "./styles.scss";
 
 export const Form = ({ name, h2, label, inputName, tipo, nodesHtml }) => {
-    const [datos, setDatos] = useState({});
+  const [datos, setDatos] = useState({});
 
-    const handleInputChange = (e) => {
-        setDatos({
-            ...datos,
-            [e.target.name]: e.target.value,
-        });
-    };
+  const handleInputChange = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        <div className="row mt-4">
-            <div className="col"></div>
-            <div className="col-md-6 p-3 text-center background-main-div">
-                <a href="esto.html" type="button" className="btn btn-primary btn-labeled text-left mb-3">
-                    <span className="btn-label">
-                        <i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i>
-                    </span>
-                    {name}
-                </a>
-                <h2 className="text-primary mb-3 text-center">{h2}</h2>
-                <label htmlFor="patente float-left">{label}</label>
-                <input
-                    type="text"
-                    onChange={handleInputChange}
-                    name={inputName}
-                    className="form-control font-weight-bold text-center input-rodados"
-                />
-                {nodesHtml[0]}
-                <Link to="/apps/totems" className="btn btn-primary active mb-1 pull-left">
-                    <i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i>
-                    Volver
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col col-md-6 offset-md-3">
+          <div className="card background-main-div text-center">
+            <div className="card-header titulo-componente text-center">
+              <span className="card-title font-weight-bold text-white">
+                {name}
+              </span>
+            </div>
+            <div className="card-body text-center">
+              <h2 className="text-primary">{h2}</h2>
+              {/* <label htmlFor="patente">{label}</label> */}
+              <h5 className="text-primary">{label}</h5>
+              <input
+                type="text"
+                onChange={handleInputChange}
+                name={inputName}
+                className="form-control font-weight-bold text-center input-rodados"
+              />
+              {nodesHtml[0]}
+            </div>
+            <div className="card-footer">
+              <div className="btn-wrapper d-flex justify-content-between">
+                <Link
+                  to="/apps/totems"
+                  type="button"
+                  className="btn btn-primary active"
+                >
+                  <i
+                    className="fa fa-arrow-circle-o-left"
+                    aria-hidden="true"
+                  ></i>{" "}
+                  Volver
                 </Link>
                 <Link
-                    to={{ pathname: '/apps/totems/pagar/', state: { tipo: tipo, data: datos[inputName] } }}
-                    className="btn btn-primary active mb-1 pull-right"
+                  to={{
+                    pathname: "/apps/totems/pagar/",
+                    state: { tipo: tipo, data: datos[inputName] },
+                  }}
+                  className="btn btn-primary active"
                 >
-                    <i className="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
-                    Siguiente
+                  <i
+                    className="fa fa-arrow-circle-o-right"
+                    aria-hidden="true"
+                  ></i>{" "}
+                  Siguiente
                 </Link>
+              </div>
             </div>
-            <div className="col"></div>
+          </div>
         </div>
-    );
+        {/*         <div className="col-md-6 offset-md-3 p-3 text-center background-main-div card">
+          <a
+            href="esto.html"
+            type="button"
+            className="btn btn-primary btn-labeled text-left mb-3"
+          >
+            {name}
+          </a>
+          <h2 className="text-primary mb-3 text-center">{h2}</h2>
+          <label htmlFor="patente float-left">{label}</label>
+          <input
+            type="text"
+            onChange={handleInputChange}
+            name={inputName}
+            className="form-control font-weight-bold text-center input-rodados"
+          />
+          {nodesHtml[0]}
+          <Link
+            to="/apps/totems"
+            className="btn btn-primary active mb-1 pull-left"
+          >
+            <i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i>{" "}
+            Volver
+          </Link>
+          <Link
+            to={{
+              pathname: "/apps/totems/pagar/",
+              state: { tipo: tipo, data: datos[inputName] },
+            }}
+            className="btn btn-primary active mb-1 pull-right"
+          >
+            <i className="fa fa-arrow-circle-o-right" aria-hidden="true"></i>{" "}
+            Siguiente
+          </Link>
+        </div> */}
+      </div>
+    </div>
+  );
 };
-
 
 export default Form;
