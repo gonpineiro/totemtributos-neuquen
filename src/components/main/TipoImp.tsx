1ecec27128b19c08cd878e1da18816bcc9a6a3b1
Norbert Stange <n0rb3r7@outlook.com>
2021-05-26 13:33:22 -0300

    Update index main, con todos los botones de los impuestos

--- src/components/main/TipoImp.tsx
+++ src/components/main/TipoImp.tsx
@@ -5,9 +5,9 @@ const TipoImp = ({ name, url }: TipoImpType) => {
     return (
         <Link to={url} type="button" className="btn btn-primary btn-labeled text-left mb-1">
             <span className="btn-label">
-                <i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i>
-                {name}
+                <i className="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
             </span>
+            {' ' + name}
         </Link>
     )
 }
