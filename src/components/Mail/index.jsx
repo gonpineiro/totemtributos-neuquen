03fc2972dae63ae987dc3d7a1803a9e93662fa69
SaniLubary <santiago.lp.cop@gmail.com>
2021-05-27 14:37:11 -0300

    update para test en replica de sendMail

--- src/components/Mail/index.jsx
+++ src/components/Mail/index.jsx
@@ -34,7 +34,7 @@ export const Mail = ({
     const callSendMail = event => {
         let mail = document.getElementById('mail').value;
         sendMail(mail).then((response) => {
-            console.log('A:',response, 'mail:', mail);
+            console.log('Response:',response);
         });
     };
     
