0aac426a0eb9677ae5310be9310457ff2112a075
Norbert Stange <n0rb3r7@outlook.com>
2021-05-31 11:49:56 -0300

    Revert "Actualización Email"
    This reverts commit 7515ce36cdfff54928ddf75412bcf4aa7874879e.

--- src/components/Mail/index.jsx
+++ src/components/Mail/index.jsx
@@ -72,7 +72,7 @@ export const Mail = ({
         } else if (datos != null && datos.error == null) {
             return (
                 <>
-                    <h3 className="font-weight-bold text-primary pb-3">Mail Enviado con éxito!</h3>
+                    <h3 className="font-weight-bold text-primary">Mail Enviado con exito!</h3>
                     <LinkBtn
                     btnClass="btn btn-primary active"
                     iClass="fa fa-arrow-circle-o-left"
@@ -85,51 +85,36 @@ export const Mail = ({
     };
 
     return (
-      <div className="row mt-5">
-        <div className="col-md-6 offset-md-3 ">
-          <div className="card background-main-div text-center">
-            <div className="card-header titulo-componente">
-              <span className="card-title font-weight-bold text-white">
-                Enviar por email
-              </span>
-            </div>
-            <div className="card-body">
-              {" "}
-              <label className="pb-3" htmlFor="patente">Ingrese su email</label>
-              <input
-                type="text"
-                id="mail"
-                className="form-control font-weight-bold"
-              />
-            </div>
-            <div className="card-footer">
-              <div className="btn-wrapper text-center d-flex justify-content-between">
-                <LinkBtn
-                  btnClass="btn btn-primary"
-                  iClass="fa fa-arrow-circle-o-left"
-                  url="/apps/totems/"
-                  desc="VOLVER"
-                />
-                <button
-                  type="button"
-                  id="enviar"
-                  onClick={checkMail}
-                  className="btn btn-success"
-                >
-                  <i className="fa fa-envelope-o" aria-hidden="true"></i> Enviar
-                </button>
-              </div>
-            </div>
-          </div>
+        <div className="row mt-4">
+            <div className="col"></div>
+            <div className="col-md-6 p-3 text-center input-wrapper">
+                <h2 className="text-primary mb-3 text-center">Enviar por mail</h2>
+                <label htmlFor="patente">Ingrese su mail</label>
+                <input type="text" id="mail" className="form-control font-weight-bold" />
+                <div className="row">
+                    <div className="col-md-12">
+                        <button type="button" id="enviar" onClick={checkMail} className="btn btn-success float-right mb-3 mt-4 col-md-3">
+                            <i className="fa fa-envelope-o" aria-hidden="true"></i> Enviar
+                    </button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <LinkBtn
+                            btnClass="btn btn-primary float-right float-right col-md-3"
+                            iClass="fa fa-arrow-circle-o-right"
+                            url="/apps/totems/"
+                            desc="VOLVER"
+                        />
+                    </div>
+                </div>
 
+            </div>
+            <div className="col"></div>
+            <div className=" flex-column align-items-center justify-content-center confirmar-mail" id="modal-print" >
+                {divCheckMail(mail)}
+            </div>
         </div>
-        <div
-          className=" flex-column align-items-center justify-content-center confirmar-mail"
-          id="modal-print"
-        >
-          {divCheckMail(mail)}
-        </div>
-      </div>
     );
 };
 
