60ad97da1ab100b80736eceb84c0d9f05d11a6df
Norbert Stange <n0rb3r7@outlook.com>
2021-06-09 10:07:07 -0300

    Actualización recibo
    - Cambio en el bloque de recycle.

--- src/components/Recibo/index.jsx
+++ src/components/Recibo/index.jsx
@@ -1,96 +1,96 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { I, LinkBtn, Cargando, Error, Recycle } from '../shared';
-import printIframe from '../utils/printIframe';
+import { I, LinkBtn, Cargando, Error, Recycle } from "../shared";
+import printIframe from "../utils/printIframe";
 
-import { recibo } from './reciboAxios';
+import { recibo } from "./reciboAxios";
 import "./recibo.scss";
 export const Recibo = ({
-    location: {
-        state: { impApagar, tr02100_id },
-    },
+  location: {
+    state: { impApagar, tr02100_id },
+  },
 }) => {
-    const [pdf, setPdf] = useState(null);
+  const [pdf, setPdf] = useState(null);
 
-    useEffect(() => {
-        recibo(tr02100_id, impApagar).then((response) => {
-            if (response !== -1) {
-                const reader = new FileReader();
-                reader.readAsDataURL(response.blob);
-                reader.onload = () => {
-                    setPdf({
-                        pdf: reader.result,
-                        recibo: response.recibo,
-                    });
-                };
-            } else {
-                setPdf(response);
-            }
-        });
-    }, [impApagar, tr02100_id]);
+  useEffect(() => {
+    recibo(tr02100_id, impApagar).then((response) => {
+      if (response !== -1) {
+        const reader = new FileReader();
+        reader.readAsDataURL(response.blob);
+        reader.onload = () => {
+          setPdf({
+            pdf: reader.result,
+            recibo: response.recibo,
+          });
+        };
+      } else {
+        setPdf(response);
+      }
+    });
+  }, [impApagar, tr02100_id]);
 
-    if (pdf === null || undefined) return <Cargando />;
+  if (pdf === null || undefined) return <Cargando />;
 
-    if (pdf === -1) return <Error msg={'Espere unos minutos e intente nuevamente por favor'} />;
+  if (pdf === -1)
+    return <Error msg={"Espere unos minutos e intente nuevamente por favor"} />;
 
-    return (
-      <div className="container">
-        <div className="row mt-2">
-          <div className="col-md-8">
-            <iframe
-              src={pdf.pdf}
-              height="580px"
-              width="100%"
-              id="pdf"
-              title={" "}
-            ></iframe>
-          </div>
-          <div className="col-md-4">
-            <div className="card background-main-div text-center">
-              <div className="card-header titulo-componente text-center">
-                <span className="card-title font-weight-bold text-white">
-                  RECIBO
-                </span>
+  return (
+    <div className="container">
+      <div className="row mt-2">
+        <div className="col-md-8">
+          <iframe
+            src={pdf.pdf}
+            height="580px"
+            width="100%"
+            id="pdf"
+            title={" "}
+          ></iframe>
+        </div>
+        <div className="col-md-4">
+          <div className="card background-main-div text-center">
+            <div className="card-header titulo-componente text-center">
+              <span className="card-title font-weight-bold text-white">
+                RECIBO
+              </span>
+            </div>
+            <div className="card-body text-center">
+              Se generó el recibo Nro {pdf.recibo}, ¿Desea enviarlo por email o
+              imprimirlo?
+            </div>
+            <div className="card-body text-center recycle">
+              <div className="d-inline-flex">
+                <Recycle /> <span>Para pagar online escanea el código QR</span>
               </div>
-              <div className="card-body text-center">
-                Se generó el recibo Nro {pdf.recibo}, ¿Desea enviarlo por email
-                o imprimirlo?
-              </div>
-              <div className="card-body text-center">
-                <div className="d-inline-flex">
-                  <Recycle />{" "}
-                  <span>Para pagar online escanea el código QR</span>
-                </div>
-              </div>
-              <div className="card-footer">
-                <div className="btn-wrapper">
-                  <button
-                    onClick={() => printIframe("pdf")}
-                    type="button"
-                    className="btn btn-info active m-3"
-                  >
-                    <I classname="fa fa-print" /> IMPRIMIR
-                  </button>
-                  <Link
-                    to={{
-                      pathname: "/apps/totems/mail/",
-                      state: { recibo: pdf.recibo, tipo: "recibo" },
-                    }}
-                    className="btn btn-success mb-1 float-right"
-                  >
-                    <I classname="fa fa-envelope-o" /> ENVIAR POR EMAIL
-                  </Link>
-                  <LinkBtn
-                    btnClass="btn btn-primary active m-3"
-                    url="/apps/totems/"
-                    desc="VOLVER"
-                  />
-                </div>
+            </div>
+            <div className="card-footer">
+              <div className="btn-wrapper">
+                <button
+                  onClick={() => printIframe("pdf")}
+                  type="button"
+                  className="btn btn-info active m-3"
+                >
+                  <I classname="fa fa-print" /> IMPRIMIR
+                </button>
+                <Link
+                  to={{
+                    pathname: "/apps/totems/mail/",
+                    state: { recibo: pdf.recibo, tipo: "recibo" },
+                  }}
+                  className="btn btn-success mb-1 float-right"
+                >
+                  <I classname="fa fa-envelope-o" /> ENVIAR POR EMAIL
+                </Link>
+                <LinkBtn
+                  btnClass="btn btn-primary active m-3"
+                  url="/apps/totems/"
+                  desc="VOLVER"
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
