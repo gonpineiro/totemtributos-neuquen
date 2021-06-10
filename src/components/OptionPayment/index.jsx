ca35478b96ad97f0a2f37a8acd582828d9f70159
Norbert Stange <n0rb3r7@outlook.com>
2021-06-10 12:22:28 -0300

    Cambio botones en option payment

--- src/components/OptionPayment/index.jsx
+++ src/components/OptionPayment/index.jsx
@@ -26,40 +26,43 @@ export const CuotasPagar = ({
 
     if (imponible.estado_complementario === 'Caducado') return <Error msg={'Caducado'} />;
     return (
-        <div className="container">
-            <div className="row mt-5">
-                <h2 className="text-center pt-3">Seleccione una opción</h2>
-                <div className="col col-md-12 text-center">
-                    <Link
-                        to={{
-                            pathname: '/apps/totems/pagar-mensual/',
-                            state: { imponible, titles, tipo },
-                        }}
-                        className="btn btn-form active text-uppercase m-4"
-                    >
-                        Emisión/ PAGO recibos año {YEAR_NOW} (mensual)
-                    </Link>
-                </div>
-                <div className="col col-md-12 text-center">
-                    {tipo !== 'PPG' && (
-                        <Link
-                            to={{
-                                pathname: '/apps/totems/pagar-semestral/',
-                                state: { imponible },
-                            }}
-                            className="btn btn-form active text-uppercase m-4"
-                        >
-                            Reimprimir recibo 1er semestre {YEAR_NOW} (pdf)
-                        </Link>
-                    )}
-                </div>
-                <div className="col-12 text-center mt-5">
-                    <Link to="/apps/totems" className="btn btn-form active mb-1 text-center">
-                        SALIR
-                    </Link>
-                </div>
-            </div>
+      <div className="container">
+        <div className="row mt-5">
+          <h2 className="text-center pt-3">Seleccione una opción</h2>
+          <div className="col col-md-12 text-center">
+            <Link
+              to={{
+                pathname: "/apps/totems/pagar-mensual/",
+                state: { imponible, titles, tipo },
+              }}
+              className="btn btn-form active text-uppercase m-4"
+            >
+              Generar recibos mensuales {YEAR_NOW}
+            </Link>
+          </div>
+          <div className="col col-md-12 text-center">
+            {tipo !== "PPG" && (
+              <Link
+                to={{
+                  pathname: "/apps/totems/pagar-semestral/",
+                  state: { imponible },
+                }}
+                className="btn btn-form active text-uppercase m-4"
+              >
+                Generar recibo semestral
+              </Link>
+            )}
+          </div>
+          <div className="col-12 text-center mt-5">
+            <Link
+              to="/apps/totems"
+              className="btn btn-form active mb-1 text-center"
+            >
+              SALIR
+            </Link>
+          </div>
         </div>
+      </div>
     );
 };
 
